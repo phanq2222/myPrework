a3ea730112bc96b364421de7880dfce89ec411f5
phanq2222 <88728017+phanq2222@users.noreply.github.com>
2022-08-20 14:22:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myprework/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myprework
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnHello : Button = findViewById<Button>(R.id.btnSayHello)
+        btnHello.setOnClickListener{
+
+            Log.v("Hello world!","Button clicked")
+            Toast.makeText(this, "Hello you too!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
